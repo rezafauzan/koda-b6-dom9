c9d6199e0d43a13b0facf2fa652ee52bf77df0d3
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-19 14:22:17 +0700

    Chore: Split style and script into seperated file

---
+++ assets/js/result.js
@@ -0,0 +1,25 @@
+    const data = JSON.parse(window.localStorage.getItem('survey-result'))
+    const tbody = document.querySelector('table tbody')
+    
+    data.forEach(
+        result => {
+            const tr = document.createElement('tr')
+            const fullname = document.createElement('td')
+            fullname.textContent = result.fullname
+            tr.appendChild(fullname)
+            const age = document.createElement('td')
+            age.textContent = result.age
+            tr.appendChild(age)
+            const gender = document.createElement('td')
+            gender.textContent = result.gender
+            tr.appendChild(gender)
+            const isSmoke = document.createElement('td')
+            isSmoke.textContent = result.isSmoke
+            tr.appendChild(isSmoke)
+            const cigarBrand = document.createElement('td')
+            cigarBrand.textContent = result['cigar-brand']
+            tr.appendChild(cigarBrand)
+    
+            tbody.appendChild(tr)
+        }
+    )
---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const formsp = document.getElementById('form-survey-perokok')
+
+let result = []
+if (window.localStorage.getItem('survey-result')) {
+    result = JSON.parse(window.localStorage.getItem('survey-result'))
+}
+
+formsp.addEventListener('submit',
+    e => {
+        e.preventDefault()
+        const formData = new FormData(formsp)
+        const data = Object.fromEntries(formData.entries())
+        result.push(data)
+        window.localStorage.setItem('survey-result', JSON.stringify(result))
+        window.location.href = 'result.html'
+    }
+)
